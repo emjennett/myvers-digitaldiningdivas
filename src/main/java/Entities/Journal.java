670a610c94a5292c8ec9b910c714cd1075543f24
emjennett <emily.jennett@mail.utoronto.ca>
2022-11-19 14:29:44 -0500

    Further developed the Restaurant use case; added simple GUI in Main. Had to resolve conflicts in Journal class.

--- src/main/java/Entities/Journal.java
+++ src/main/java/Entities/Journal.java
@@ -1,6 +1,53 @@
 package Entities;
 
-import java.io.Serializable;
+// 7-Journal-Creation
+import APP_Business_Rules.UserDataGrabber;
 
-public class Journal implements Serializable {
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Journal {
+    private String name; // Name of Journal Owner
+
+    private Account userAccount; // account of Journal Owner
+
+    private List<Review> reviews = new ArrayList<>(); // List of reviews of dish
+
+    private Date createdOn; // Date that the Journal was created on.
+
+
+    public Journal(String name, Account Account) {
+        this.name = name;
+        this.userAccount = Account;
+        this.createdOn = new Date();
+
+    }
+
+    public String pullName(String name) {
+        return this.name;
+
+    }
+
+    public Account pullAccount(String name) {
+        return this.userAccount;
+    }
+
+    public String getCreatedOn() {
+        //Returns the date which the Journal was created on in string form.
+        SimpleDateFormat formatted = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        return formatted.format(this.createdOn);
+
+    }
+//    public List<Review> pullInformation(){
+//        // returns all of the reviews corresponding to account
+//        // this is a work in progress as user data grabber is nodefined yet
+//        // I'm assuming that user data grabber returns a arraylist of reviews
+//
+//        // List<Review> allreviews = UserDataGrabber();
+//        // return allreviews;
+//    }
+
+
 }
