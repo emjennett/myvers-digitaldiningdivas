470c79e60dfc5abd5752f967f04b72e6f10ed05b
LJCar <ljcaridi@yahoo.com>
2022-11-20 23:52:07 -0500

    Use Case: Create User: Completed code with its own gateway and data storing system. This Commit does not include UI.

--- src/main/java/Entities/Account.java
+++ src/main/java/Entities/Account.java
@@ -6,26 +6,29 @@ import java.util.Date;
 import java.util.Objects;
 
 public abstract class Account implements Serializable {
-    //Abstract class which functions as the base for all account types.
-    private String UserName; //The username associated with the account.
-    private final String password; //The password associated with the account.
+    //A class which functions as the base for all account types.
+    private String userName; //The username associated with the account.
+    private String password; //The password associated with the account.
     private Date createdOn; //Date of the accounts' creation.
 
     public Account(String name, String pass){
         //Constructor for the account class.
-        this.UserName = name;
+        this.userName = name;
         this.password = pass;
         this.createdOn = new Date();
     }
 
     public String getUserName(){
         //Returns the username of the account.
-        return this.UserName;
+        return this.userName;
     }
-
+    public String getPassword(){
+        //Returns the password of the account.
+        return this.password;
+    }
     public boolean confirmPassword(String pass){
         //Returns a boolean. True if the given string matches the account password, false otherwise.
-        return (Objects.equals(this.password, pass));
+        return (Objects.equals(this.getPassword(), pass));
     }
 
     public String getCreatedOn(){
--- src/main/java/Entities/AccountUser.java
+++ src/main/java/Entities/AccountUser.java
@@ -1,7 +1,14 @@
 package Entities;
 
-public class AccountUser extends Account{
+import java.util.Objects;
+
+public class AccountUser extends Account implements User {
     public AccountUser(String name, String pass) {
         super(name, pass);
+    }
+
+    @Override
+    public boolean validPassword() {
+        return this.getPassword().length() >= 6;
     }
 }
